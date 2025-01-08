64d740d29e5060c2160bf637cdeeedb4b231d861
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2025-01-08 19:40:57 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -64,7 +64,7 @@ app.get("/prof", isAuthenticated, (req, res) => {
 app.get("/blekbok", limit, async (req, res) => {
     require("../pages/fitures/blackbox.js")(req, res)
 })
-app.get("/dalle", limit, async (req, res) => {
+app.get("/txtimg", limit, async (req, res) => {
     require("../pages/fitures/dale.js")(req, res)
 })
 app.get("/iplookup", limit, async (req, res) => {
