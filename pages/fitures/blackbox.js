556be02e54d9c54d0c4c6cfa400f713a60ea9062
kaizel12 <134833137+kaizel12@users.noreply.github.com>
2025-01-11 19:39:28 +0700

    Update blackbox.js

--- pages/fitures/blackbox.js
+++ pages/fitures/blackbox.js
@@ -1,21 +1,23 @@
 /*
- * by kaizel jskai
+ * by balxzzy
  * don't delete my wm
- * follow more on Instagram: @iqstore78
+ * follow more on Instagram: @balxzzy
  */
-const axios = require("axios");
-const allowedApiKeys = require("../../declaration/arrayKey.jsx");
+const Groq = require('groq-sdk');
+const allowedApiKeys = require('../../declaration/arrayKey.jsx'); // Daftar API key yang diizinkan
 
 // Fungsi untuk menghasilkan random RID
 function generateRandomRid() {
   return Math.random().toString(36).substring(2, 15);
 }
 
-module.exports = async (req, res) => {
-  const q = req.query.q || ""; // Query dari user
-  const apiKey = req.query.apiKey; // API Key
+const client = new Groq({ apiKey: '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' });
 
-  if (!q) {
+module.exports = async (req, res) => {
+  const prompt = req.query.prompt || ""; // Prompt atau pertanyaan dari user
+  const apiKey = req.query.apiKey; // API key
+
+  if (!prompt) {
     return res.status(400).json({
       error: "Mau nanya apa lu njir",
     });
@@ -23,31 +25,28 @@ module.exports = async (req, res) => {
 
   if (!apiKey || !allowedApiKeys.includes(apiKey)) {
     return res.status(403).json({
-      error: "Input Parameter Apikey!",
+      error: "API key tidak valid atau tidak disediakan!",
     });
   }
 
   const rid = generateRandomRid(); // Generate random RID
-  const url = `https://search.lepton.run/api/query`;
-
-  const requestData = {
-    query: q,
-    rid: rid,
-  };
 
   try {
-    const response = await axios.post(url, requestData, {
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const chatCompletion = await client.chat.completions.create({
+      messages: [
+        { role: "system", content: "Kamu Adalah kaizel kazami Dengan Memakai Bahasa Indonesia Dan Bergaulan" },
+        { role: "user", content: prompt },
+      ],
+      model: 'llama3-8b-8192',
     });
 
-    const data = response.data;
+    const hasil = chatCompletion.choices[0].message.content;
 
     res.status(200).json({
-      creator: "kaizel jskai", // Nama kreator
-      question: q, // Pertanyaan dari user
-      answer: data.result || "Tidak ada jawaban yang tersedia", // Jawaban dari API
+      creator: "kaizel kaijs", // Nama kreator
+      question: prompt, // Pertanyaan dari user
+      answer: hasil || "Tidak ada jawaban yang tersedia", // Jawaban dari API
+      rid: rid, // RID untuk pelacakan
     });
   } catch (error) {
     res.status(500).json({
